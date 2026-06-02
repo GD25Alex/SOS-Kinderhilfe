21bb5ca32792c74433bfb5e58bef6ff8fbc1b13b
GD25Alex <gd25.akrauss@stud.fh-dresden.eu>
2026-06-02 19:21:53 +0200

    erster commit

---
+++ web/app/app.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="page-wrapper">
+    <div class="site-layout">
+      <Header />
+      <main class="main-content">
+        <NuxtPage />
+      </main>
+      <Footer />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.page-wrapper {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  background-color: var(--bg-color);
+  overflow-x: hidden;
+}
+
+.site-layout {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 1200px;
+  min-height: 100vh;
+}
+
+.main-content {
+  flex-grow: 1;
+  padding: 0 20px;
+}
+</style>
---
+++ web/app/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>SOS Wohnungslosenhilfe e.V.</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt accusamus placeat deserunt molestias porro eius
+      quidem unde, at neque delectus perspiciatis deleniti voluptatibus fuga praesentium? Quibusdam consequuntur rem,
+      itaque nihil sint placeat iste ducimus laudantium dolorem voluptatum rerum ipsum reiciendis nesciunt omnis
+      exercitationem! <strong>Aliquam mollitia itaque quas voluptates, </strong> laudantium ullam debitis eum tempore id
+      amet cum
+      blanditiis a at, quos rerum praesentium deleniti. Recusandae expedita nisi culpa voluptatibus. Illum, nesciunt
+      pariatur dolore quaerat possimus cum inventore quam nesciunt. Dignissimos deserunt dolorem reprehenderit assumenda
+      non, perspiciatis nisi. Inventore, maxime labore. Dolor tempora quo minima corrupti quisquam?
+    </p>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt accusamus placeat deserunt molestias porro eius
+      quidem unde, at neque delectus perspiciatis deleniti voluptatibus fuga praesentium? Quibusdam consequuntur rem,
+      itaque nihil sint placeat iste ducimus laudantium dolorem voluptatum rerum ipsum reiciendis nesciunt omnis
+      exercitationem! <strong>Aliquam mollitia itaque quas voluptates, </strong> laudantium ullam debitis eum tempore id
+    </p>
+
+  </div>
+</template>
